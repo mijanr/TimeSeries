f653eed346edd99072a40fc17025e653af1c8a6e
mijanr <mijanr@users.noreply.github.com>
2023-08-17 07:32:54 +0000

    :art: Format Python code with psf/black

--- test.py
+++ test.py
@@ -2,13 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def calculate(
-        **kwargs
-):
+
+def calculate(**kwargs):
     # return summation of all the arguments
     return sum(kwargs.values())
 
 
-if __name__ == '__main__':
-    dic = {'a': 1, 'b': 2, 'c': 3}
+if __name__ == "__main__":
+    dic = {"a": 1, "b": 2, "c": 3}
     print(calculate(**dic))
